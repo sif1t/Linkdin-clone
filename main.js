e7bdfcfce8f98a07d408023be255087001f6f3cf
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-10-31 19:14:34 +0600

    lindin-clone now working

--- main.js
+++ main.js
@@ -8,5 +8,12 @@ let sideActivity = document.getElementById("sidebarActivity");
 let moreLink = document.getElementById("showMoreLink");
 
 function toggleActivity(){
-    sideActivity.classList.toggle("open-activity")
+    sideActivity.classList.toggle("open-activity");
+
+    if(sideActivity.classList.contains("open-activity")){
+        moreLink.innerHTML="Show less <b>-</b>";
+    }
+    else{
+        moreLink.innerHTML="Show more <b>+</b>";
+    }
 }
