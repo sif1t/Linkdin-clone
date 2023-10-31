c99d4388bdeef7eeeb178834eefa029b775150b3
arifeensifat01 <arifeen.sifat01@gmail.com>
2023-10-31 19:06:31 +0600

    add new section show more option

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ let profilemenu =document.getElementById("profilemenu");
 function toggleMenu() {
     profilemenu.classList.toggle("open-menu");
 }
+
+let sideActivity = document.getElementById("sidebarActivity");
+let moreLink = document.getElementById("showMoreLink");
+
+function toggleActivity(){
+    sideActivity.classList.toggle("open-activity")
+}
